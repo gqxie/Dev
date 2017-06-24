222099562bc8549ca416c4fa28914836ede9d16c
gqxie <xie.coder@gmail.com>
2017-06-24 14:24:54 +0800

    配置viewResolver

--- src/com/gqxie/controller/CostListController.java
+++ src/com/gqxie/controller/CostListController.java
@@ -5,9 +5,9 @@
  * Date:2017年6月24日上午9:47:04  
  * Copyright (c) 2017, xie.coder@gmail.com All Rights Reserved.  
  *  
-*/  
-  
-package com.gqxie.controller;  
+*/
+
+package com.gqxie.controller;
 /**  
  * ClassName:CostListController <br/>  
  * Function: TODO ADD FUNCTION. <br/>  
@@ -19,11 +19,30 @@ package com.gqxie.controller;
  * @see        
  */
 
-import com.gqxie.dao.ICostDao;
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gqxie.dao.ICostDao;
+import com.gqxie.entity.Cost;
+
+@Controller
+@RequestMapping("/cost")
 public class CostListController
 {
+    @Resource
     private ICostDao costDao;
-    
+
+    @RequestMapping("/list.do")
+    public String execute(ModelMap model)
+    {
+        List<Cost> list = costDao.findAll();
+        model.put("costs", list);
+        return "fee/fee_list";
+    }
+
 }
-  
---
+++ src/com/gqxie/test/HelloAnt.java
@@ -0,0 +1,39 @@
+/**  
+ * Project Name:Dev  
+ * File Name:HelloAnt.java  
+ * Package Name:com.gqxie.test  
+ * Date:2017年6月24日下午1:47:22  
+ * Copyright (c) 2017, xie.coder@gmail.com All Rights Reserved.  
+ *  
+*/  
+  
+package com.gqxie.test;  
+/**  
+ * ClassName:HelloAnt <br/>  
+ * Function: TODO ADD FUNCTION. <br/>  
+ * Reason:   TODO ADD REASON. <br/>  
+ * Date:     2017年6月24日 下午1:47:22 <br/>  
+ * @author   xie  
+ * @version    
+ * @since    JDK 1.8  
+ * @see        
+ */
+public class HelloAnt
+{
+
+    /**  
+     * main:(这里用一句话描述这个方法的作用). <br/>  
+     *  
+     * @author xie  
+     * @param args  
+     * @since JDK 1.8  
+     */
+    public static void main(String[] args)
+    {
+
+        // TODO Auto-generated method stub  
+        System.out.println("Hello ant,by zdz!");
+    }
+
+}
+  
