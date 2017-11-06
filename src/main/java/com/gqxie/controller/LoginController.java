f76e609c4d0480c3ad77e4e8e9c277d28bcb8238
xieguoqiang <xieguoqiang@mychebao.com>
2017-11-06 11:18:09 +0800

    响应信息中时间字段格式化

--- src/main/java/com/gqxie/controller/LoginController.java
+++ src/main/java/com/gqxie/controller/LoginController.java
@@ -19,25 +19,38 @@ package com.gqxie.controller;
  * @see        
  */
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gqxie.dao.IUserDao;
 import com.gqxie.entity.User;
 
+@RequestMapping("/user")
 @Controller
 public class LoginController
 {
     @Resource
     private IUserDao userDao;
 
+    private Logger   logger = Logger.getLogger(LoginController.class);
+
+    /**
+     * 用户登录
+     * @param request
+     * @return
+     */
     @RequestMapping("/login.do")
     public ModelAndView execute(HttpServletRequest request)
     {
+        logger.warn("login.do begin...");
         String account = request.getParameter("username");
         String password = request.getParameter("password");
         User user = userDao.verify(account, password);
@@ -46,7 +59,35 @@ public class LoginController
         String view = null == user ? "login" : "success";
         mav.setViewName(view);
         mav.getModel().put("msg", msg);
+        logger.warn("login.do end...");
         return mav;
+    }
+
+    /**
+     * 通过userID查询指定用户
+     * @param request
+     * @return
+     */
+    @RequestMapping("getUserByUserID")
+    @ResponseBody
+    private Object getUserByUserID(HttpServletRequest request)
+    {
+        Long userID = Long.valueOf(request.getParameter("userID"));
+        User user = userDao.findByUserID(userID);
+        return user;
+    }
+
+    /**
+     * 查询所有用户
+     * @param request
+     * @return
+     */
+    @RequestMapping("findAll")
+    @ResponseBody
+    private Object findAll(HttpServletRequest request)
+    {
+        List<User> list = userDao.findAll();
+        return list;
     }
 
 }
