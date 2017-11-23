77cbf9c6ab265b17a73530031dddb972508227d7
xieguoqiang <xieguoqiang@mychebao.com>
2017-11-23 15:47:43 +0800

    mysql与activemq使用远程地址

--- db/create_table.sql
+++ db/create_table.sql
@@ -1,6 +1,40 @@
-DROP TABLE IF EXISTS t_user;
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本地mysql
+Source Server Version : 50637
+Source Host           : 127.0.0.1:3306
+Source Database       : dev
+
+Target Server Type    : MYSQL
+Target Server Version : 50637
+File Encoding         : 65001
+
+Date: 2017-11-23 15:01:44
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_app
+-- ----------------------------
+DROP TABLE IF EXISTS `t_app`;
+CREATE TABLE `t_app` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `appname` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  `updatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
-  `userID` bigint(20) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` bigint(20) DEFAULT NULL,
   `account` varchar(255) DEFAULT NULL,
   `pwd` varchar(255) DEFAULT NULL,
   `gender` tinyint(4) DEFAULT NULL,
@@ -10,11 +44,5 @@ CREATE TABLE `t_user` (
   `email` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `nickName` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-
-
-
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
